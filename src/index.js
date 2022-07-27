20a60ab30d0e5f7b5f63f955b79b130f0a381163
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-07-27 17:19:28 +0200

    fixing linters

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import './index.css';
 import loadShows from './loadShows.js';
 import commentspopup from './commentspopup.js';
-// import addShowToDom from './addShowToDom';
 
 loadShows().then((value) => {
-  // console.log(value);
   commentspopup(value, '812');
 });
 // (error) => { error; });
