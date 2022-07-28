f01735180f0282fbb07e4c6d3d29c7ce4295df44
Hamma Azarok <hammaagazarok@gmail.com>
2022-07-28 22:52:38 +0200

    add test for shows counter

---
+++ src/countItems.test.js
@@ -0,0 +1,32 @@
+import countShows from "./showsCounter.js";
+
+let shows = {};
+
+describe("Testing Shows", () => {
+  describe("Testing Shows Counter", () => {
+    test("Shows are not defined", async () => {
+      shows = undefined;
+      const result = countShows(shows);
+      expect(result).toBe("shows are not defined");
+    });
+    test("Shows are not empty", async () => {
+      shows = [];
+      const result = countShows(shows);
+      expect(result).toBe("there is no show yet on this category");
+    });
+    test("Shows count is 2", async () => {
+      shows = [
+        {
+          name: "test1",
+          id: 0,
+        },
+        {
+          name: "test2",
+          id: 1,
+        },
+      ];
+      const result = countShows(shows);
+      expect(result).toBe(2);
+    });
+  });
+});
