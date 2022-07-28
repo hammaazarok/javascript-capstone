011e6aa970620396b2b0ba15c13ade72ba818d07
Hamma Azarok <hammaagazarok@gmail.com>
2022-07-28 22:54:00 +0200

    fix lintes

--- src/countItems.test.js
+++ src/countItems.test.js
@@ -1,27 +1,27 @@
-import countShows from "./showsCounter.js";
+import countShows from './showsCounter.js';
 
 let shows = {};
 
-describe("Testing Shows", () => {
-  describe("Testing Shows Counter", () => {
-    test("Shows are not defined", async () => {
+describe('Testing Shows', () => {
+  describe('Testing Shows Counter', () => {
+    test('Shows are not defined', async () => {
       shows = undefined;
       const result = countShows(shows);
-      expect(result).toBe("shows are not defined");
+      expect(result).toBe('shows are not defined');
     });
-    test("Shows are not empty", async () => {
+    test('Shows are not empty', async () => {
       shows = [];
       const result = countShows(shows);
-      expect(result).toBe("there is no show yet on this category");
+      expect(result).toBe('there is no show yet on this category');
     });
-    test("Shows count is 2", async () => {
+    test('Shows count is 2', async () => {
       shows = [
         {
-          name: "test1",
+          name: 'test1',
           id: 0,
         },
         {
-          name: "test2",
+          name: 'test2',
           id: 1,
         },
       ];
